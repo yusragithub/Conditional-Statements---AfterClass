0ec0c576c86665d40efd85f415d00e1ebd8fcd1d
yusragithub <yusrayahya763@gmail.com>
2025-01-01 12:38:08 +0500

    conditional-statements

--- conditional-statement.py
+++ conditional-statement.py
@@ -3,7 +3,11 @@ attendance = int(input("Enter your attendance: "))
 if number > 60 :
     if attendance > 75 :
         print("You are pass")
-    elif attendance >= 75 :
+    elif attendance == 75 :
         print("You secured passing marks")
+    else :
+        print("You secured passing marks but didn't secure passing attendance")
+elif number == 60 :
+    print("You secured passing marks")
 else :
-    print("You didn't secure passing marks")
+    print("You didn't secured passing marks")
